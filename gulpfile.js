e7752bb1d2b967cb0a414ff06bb2a7766c4a2776
MaksimBorovik <borovik.maks@gmail.com>
2019-07-09 02:54:29 +0300

    added content-main__tiles

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 const {task, series, parallel, src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
-const notify = require('gulp-notify');
-const cssnano = require('cssnano');
-const rename = require('gulp-rename');
-const postcss = require('gulp-postcss');
-const csscomb = require('gulp-csscomb');
-const autoprefixer = require('autoprefixer');
-const mqpacker = require('css-mqpacker');
-const sortCSSmq = require('sort-css-media-queries');
 
 const path = {
   scssFolder: './assets/scss/',
@@ -20,71 +12,11 @@ const path = {
   jsFiles: './assets/js/**/*.js'
 };
 
-const plugins = [
-  autoprefixer({
-    overrideBrowserslist: [
-      'last 5 versions',
-      '> 1%'
-    ],
-    cascade: true
-  }),
-  mqpacker({sort: sortCSSmq})
-];
-
 function scss() {
   return src(path.scssFile).
     pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    // pipe(postcss(plugins)).
-    pipe(dest(path.cssFolder)).
-    pipe(notify({
-      message: 'Compiled!',
-      sound: false
-    })).
-    pipe(browserSync.reload({stream: true}));
-}
-
-function scssDev() {
-  return src(path.scssFile, {sourcemaps: true}).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(plugins)).
-    pipe(dest(path.cssFolder, {sourcemaps: true})).
-    pipe(notify({
-      message: 'Compiled!',
-      sound: false
-    })).
-    pipe(browserSync.reload({stream: true}));
-}
-
-function cssMin() {
-  const pluginsExtended = plugins.concat([cssnano({preset: 'default'})]);
-
-  return src(path.cssFile).
-    pipe(postcss(pluginsExtended)).
-    pipe(rename({suffix: '.min'})).
     pipe(dest(path.cssFolder)).
     pipe(browserSync.reload({stream: true}));
-}
-
-function scssMin() {
-  const pluginsExtended = plugins.concat([cssnano({preset: 'default'})]);
-
-  return src(path.scssFile).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(pluginsExtended)).
-    pipe(rename({suffix: '.min'})).
-    pipe(dest(path.cssFolder)).
-    pipe(notify({
-      message: 'Compiled!',
-      sound: false
-    })).
-    pipe(browserSync.reload({stream: true}));
-}
-
-function comb() {
-  return src(path.scssFiles).
-    pipe(csscomb()).
-    on('error', notify.onError((error) => `File: ${error.message}`)).
-    pipe(dest(path.scssFolder));
 }
 
 function syncInit() {
@@ -100,13 +32,9 @@ async function sync() {
 
 function watchFiles() {
   syncInit();
-  watch(path.scssFiles, scss);
+  watch(path.scssFiles, series(scss));
   watch(path.htmlFiles, sync);
-  watch(path.jsFiles, sync);
 }
 
-task('comb', comb);
-task('scss', series(scss, cssMin));
-task('min', scssMin);
-task('dev', scssDev);
+task('scss', series(scss));
 task('watch', watchFiles);
